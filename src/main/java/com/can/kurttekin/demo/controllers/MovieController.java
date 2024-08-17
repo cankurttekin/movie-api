3e6f38039b3b1962e93ba90ab5112e0191897f6b
can kurttekin <cankurttekin@gmail.com>
2024-08-17 23:49:10 +0300

    controller returns movie list

--- src/main/java/com/can/kurttekin/demo/controllers/MovieController.java
+++ src/main/java/com/can/kurttekin/demo/controllers/MovieController.java
@@ -1,16 +1,25 @@
 package com.can.kurttekin.demo.controllers;
 
+import com.can.kurttekin.demo.domain.entity.Movie;
+import com.can.kurttekin.demo.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
     @GetMapping
-    public ResponseEntity<String> getMovies() {
-        return new ResponseEntity<String>("Movies", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.getAllMovies(), HttpStatus.OK);
     }
 }
