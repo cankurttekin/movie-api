134340d70cab33ffbb47e27d2e3cb8013630b41a
can kurttekin <cankurttekin@gmail.com>
2024-08-18 00:02:45 +0300

    getmovie(s) controller

--- src/main/java/com/can/kurttekin/demo/controllers/MovieController.java
+++ src/main/java/com/can/kurttekin/demo/controllers/MovieController.java
@@ -2,14 +2,17 @@ package com.can.kurttekin.demo.controllers;
 
 import com.can.kurttekin.demo.domain.entity.Movie;
 import com.can.kurttekin.demo.service.MovieService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -21,5 +24,15 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<List<Movie>> getMovies() {
         return new ResponseEntity<List<Movie>>(movieService.getAllMovies(), HttpStatus.OK);
+    }
+//      FIND BY OBJECT ID REMOVED TO NOT EXPOSE IDs INSTEAD IMDBID MAPPING BELOW
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Optional<Movie>> getMovie(@PathVariable ObjectId id) {
+//        return new ResponseEntity<Optional<Movie>>(movieService.findMovieById(id), HttpStatus.OK);
+//    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getMovie(@PathVariable String  imdbId) {
+        return new ResponseEntity<Optional<Movie>>(movieService.findMovieByImdbId(imdbId), HttpStatus.OK);
     }
 }
--- src/main/java/com/can/kurttekin/demo/service/MovieService.java
+++ src/main/java/com/can/kurttekin/demo/service/MovieService.java
@@ -2,10 +2,12 @@ package com.can.kurttekin.demo.service;
 
 import com.can.kurttekin.demo.domain.entity.Movie;
 import com.can.kurttekin.demo.repository.MovieRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -14,5 +16,13 @@ public class MovieService {
 
     public List<Movie> getAllMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> findMovieById(ObjectId id) {
+        return Optional.ofNullable(movieRepository.findById(id).orElse(null));
+    }
+
+    public Optional<Movie> findMovieByImdbId(String imdbId) {
+        return Optional.ofNullable(movieRepository.findMovieByImdbId(imdbId).orElse(null));
     }
 }
